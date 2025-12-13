695b451b6d2899d607346053686ba6c918cedef6
dunt21 <suadmacaulay50@gmail.com>
2025-12-13 20:16:21 +0000

    ff

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+    <div className="px-5 max-w-screen min-h-screen">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import logo from "../assets/logo.svg";
+import { BiMenu } from "react-icons/bi";
+
+export default function Navbar() {
+  const navItems = ["Features", "Pricing", "Resources"];
+
+  return (
+    <div className="pt-8 flex justify-between items-center relative">
+      <img src={logo} />
+      <div>
+        <BiMenu className="text-gray-500 " size={40} />
+        <div className="absolute left-0 bg-purple-950 text-white w-full py-8 px-5 text-center font-bold border rounded-xl text-lg mt-5 hidden">
+          <div>
+            <ul>
+              {navItems.map((el, i) => (
+                <li key={i} className="mb-6">
+                  <a>{el}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="space-y-6">
+            <div className="border-[0.1px] border-gray-500"></div>
+
+            <p>
+              <a>Login</a>
+            </p>
+            <button>Sign up</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
